a9cbc8a9b066b3f13421ae36e684ad71ae71dd05
Béres Balázs <b5.balazs98@gmail.com>
2020-05-01 23:17:44 +0200

    Initial Commit

---
+++ grafika-3d-enginelet/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  grafika-3d-enginelet
+//
+//  Created by Béres Balázs on 2020. 05. 01..
+//  Copyright © 2020. Béres Balázs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
